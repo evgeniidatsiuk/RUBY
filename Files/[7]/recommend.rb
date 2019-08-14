97950c1bbe7aaaa4e1ec6a5f9e0cae014ae33763
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-08-14 22:06:56 +0300

    Add Math

--- Files/[7]/recommend.rb
+++ Files/[7]/recommend.rb
@@ -3,7 +3,7 @@ current_path = File.dirname(__FILE__)
 file_path = current_path + '/data/movies.txt'
 
 if File.exists?(file_path)
-  file = File.new(file_path)
+  file = File.new(file_path, chomp: true)
   movies = file.readlines
   file.close
   index = rand(movies.length)
---
+++ Math/[1]/main.rb
@@ -0,0 +1,7 @@
+pi = Math::PI
+e =  Math::E
+
+puts "Число e = #{pi}"
+puts "Число pi = #{e}"
+
+puts "Синус pi + косинус pi = #{(Math.sin(pi) + Math.cos(pi)).round(2)}"
